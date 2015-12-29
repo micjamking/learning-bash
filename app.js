493744cc7d5c6dfcd035b6bf554d036f96b5fa99
Mike King <yo@mikeking.io>
2015-12-29 00:08:06 -1000

    Adding express to serve files

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var tarball = require('tarball-extract');
 var rimraf  = require('rimraf');
 var colors  = require('colors');
 var fs      = require('fs');
+var express = require('express');
+
+var app = express();
 
 /** @private */
 var urls = [
@@ -15,28 +18,37 @@ var urls = [
   'http://www.tldp.org/LDP/abs/abs-guide.html.tar.gz'
 ];
 
-console.log(''); // Create some space after prompt
+function loadPage(req, res){
+  
+  console.log(''); // Create some space after prompt
+  
+  urls.forEach( function( url ) {
 
-urls.forEach( function( url ) {
+    console.log('Fetching ' + url.blue + '\n');
 
-  console.log('Fetching ' + url.blue + '\n');
+    var tarname  = url.substring( url.lastIndexOf( '/' ) + 1 ),
+        filename = tarname.substring( 0, tarname.indexOf( '.' ) );
 
-  var tarname  = url.substring( url.lastIndexOf( '/' ) + 1 ),
-      filename = tarname.substring( 0, tarname.indexOf( '.' ) );
-
-  function callback( err, result ){
-    if ( !err ) {
-      // Remove *tar.gz
-      rimraf(tarname, function() { 
-        // Rename file
-        fs.rename(filename, filename.toLowerCase(), function() {
-          console.log( 'Downloaded ' + filename.toLowerCase().green + ' successfully!\n' );
+    function callback( err, result ){
+      if ( !err ) {
+        // Remove *tar.gz
+        rimraf(tarname, function() { 
+          // Rename file
+          fs.rename(filename, filename.toLowerCase(), function() {
+            console.log( 'Downloaded ' + filename.toLowerCase().green + ' successfully!\n' );
+          });
         });
-      });
-    } else { 
-      console.log( err );
+      } else { 
+        console.log( err );
+      }
     }
-  }
 
-  tarball.extractTarballDownload( url, tarname, './', {}, callback);
-});
+    tarball.extractTarballDownload( url, tarname, './', {}, callback);
+  });
+
+  res.send('Check the console...')
+}
+
+app.get('/', loadPage);
+app.listen('8000')
+exports = module.exports = app;
