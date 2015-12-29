32477b4c8e4d442e8f6a45ca5be2afb6513f226d
Mike King <yo@mikeking.io>
2015-12-29 01:44:24 -1000

    Updating call of loadPages

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ function getPages(files, directory){
 
 app.get('/', function(req, res){
   res.send('<a href="/abs-guide">Advanced Bash Scripting Guide</a>\n<a href="/bash-beginners-guide">Bash Guide for Beginners</a>');
-  loadPage();
+  //loadPage();
 });
 
 app.listen('8000')
